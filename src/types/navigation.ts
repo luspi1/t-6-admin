e6a5d6a7678355479ac6af34ad59243f470d56c3
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-07-09 17:42:38 +0300

    init

---
+++ src/types/navigation.ts
@@ -0,0 +1,19 @@
+import { type ReactNode } from 'react'
+
+export type NavigationItem = {
+	title: string
+	link: string
+	accent?: boolean
+	icon?: ReactNode
+	childItems?: NavigationItem[]
+}
+
+export type ContentNav = {
+	linkIdx: number
+}
+
+export type TabNavigationItem = {
+	title: string
+	link: string
+	exact?: boolean
+}
